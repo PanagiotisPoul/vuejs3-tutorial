b684a3c8e698a07d816a991c88234b4289e741ec
PanagiotisPoul <Panagiotis@xweb.gr>
2025-03-01 16:46:05 +0200

    first modal-project commit

---
+++ modal-project/src/App copy.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div>
+    <input type="text" ref="boo">
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "My First View App :)"
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log('test:', this.$refs.boo)
+      this.$refs.boo.classList.add('active')
+      this.$refs.boo.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal :theme="theme" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Enter contest</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>PoulPan</h1>
+      <p>Hello friend!</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="loginRegister">
+    <Modal @close="toggleLoginRegister">
+      <template v-slot:login>
+        <a href="#">Login</a>
+        <a href="#">SignUp</a>
+      </template>
+      <h1>Login/Register</h1>
+      <p>Not a user? Please register first!</p>
+    </Modal>
+  </teleport>
+
+  <button @click.shift="toggleModal">open modal (shift)</button>
+  <button @click="toggleLoginRegister">Login/Register</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: "My First View App :)",
+      theme: "sale",
+      showModal: false,
+      loginRegister: false
+    }
+  },
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal;
+      },
+      toggleLoginRegister() {
+        this.loginRegister = !this.loginRegister;
+      }
+    }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
